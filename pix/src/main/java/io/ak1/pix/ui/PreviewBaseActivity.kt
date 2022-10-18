635e47349653daf6eaff47e3f2e7ea06b378855a
Wang Jian <coderjian@gmail.com>
2022-10-18 11:27:20 +0800

    add checkview for preview activity.

--- pix/src/main/java/io/ak1/pix/ui/PreviewBaseActivity.kt
+++ pix/src/main/java/io/ak1/pix/ui/PreviewBaseActivity.kt
@@ -33,6 +33,7 @@ import io.ak1.pix.databinding.ActivityPreviewBinding
 import io.ak1.pix.livedata.MediaLiveData
 import io.ak1.pix.models.Img
 import io.ak1.pix.models.ModelList
+import io.ak1.pix.ui.widget.CheckView
 
 /**
  * Shows how to use notifyDataSetChanged with [ViewPager2]
@@ -42,12 +43,12 @@ abstract class PreviewBaseActivity : FragmentActivity() {
 
     private lateinit var binding: ActivityPreviewBinding
 
-    private lateinit var buttonAddAfter: Button
-    private lateinit var buttonAddBefore: Button
-    private lateinit var buttonGoTo: Button
-    private lateinit var buttonRemove: Button
-    private lateinit var itemSpinner: Spinner
-    private lateinit var checkboxDiffUtil: CheckBox
+//    private lateinit var buttonAddAfter: Button
+//    private lateinit var buttonAddBefore: Button
+//    private lateinit var buttonGoTo: Button
+//    private lateinit var buttonRemove: Button
+//    private lateinit var itemSpinner: Spinner
+//    private lateinit var checkboxDiffUtil: CheckBox
     private lateinit var viewPager: ViewPager2
     private lateinit var previewAdapter: PreviewPagerAdapter
 
@@ -56,12 +57,12 @@ abstract class PreviewBaseActivity : FragmentActivity() {
         binding = ActivityPreviewBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        buttonAddAfter = binding.buttonAddAfter
-        buttonAddBefore = binding.buttonAddBefore
-        buttonGoTo = binding.buttonGoTo
-        buttonRemove = binding.buttonRemove
-        itemSpinner = binding.itemSpinner
-        checkboxDiffUtil = binding.useDiffUtil
+//        buttonAddAfter = binding.buttonAddAfter
+//        buttonAddBefore = binding.buttonAddBefore
+//        buttonGoTo = binding.buttonGoTo
+//        buttonRemove = binding.buttonRemove
+//        itemSpinner = binding.itemSpinner
+//        checkboxDiffUtil = binding.useDiffUtil
         viewPager = binding.viewPager
         viewPager.offscreenPageLimit = 2
 
@@ -69,38 +70,47 @@ abstract class PreviewBaseActivity : FragmentActivity() {
         viewPager.adapter = previewAdapter
 
 
-        itemSpinner.adapter = object : BaseAdapter() {
-            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View =
-                ((convertView as TextView?) ?: TextView(parent.context)).apply {
-                    if (Build.VERSION.SDK_INT >= 17) {
-                        textDirection = View.TEXT_DIRECTION_LOCALE
-                    }
-                    text = getItem(position)
-                }
+//        itemSpinner.adapter = object : BaseAdapter() {
+//            override fun getView(position: Int, convertView: View?, parent: ViewGroup): View =
+//                ((convertView as TextView?) ?: TextView(parent.context)).apply {
+//                    if (Build.VERSION.SDK_INT >= 17) {
+//                        textDirection = View.TEXT_DIRECTION_LOCALE
+//                    }
+//                    text = getItem(position)
+//                }
+//
+//            override fun getItem(position: Int): String = items.getItemById(getItemId(position))
+//            override fun getItemId(position: Int): Long = items.itemId(position)
+//            override fun getCount(): Int = items.size
+//        }
 
-            override fun getItem(position: Int): String = items.getItemById(getItemId(position))
-            override fun getItemId(position: Int): Long = items.itemId(position)
-            override fun getCount(): Int = items.size
+//        buttonGoTo.setOnClickListener {
+//            viewPager.setCurrentItem(itemSpinner.selectedItemPosition, true)
+//        }
+//
+//
+//
+//        buttonRemove.setOnClickListener {
+//            changeDataSet { items.removeAt(itemSpinner.selectedItemPosition) }
+//        }
+//
+//        buttonAddBefore.setOnClickListener {
+//            changeDataSet { items.addNewAt(itemSpinner.selectedItemPosition) }
+//        }
+//
+//        buttonAddAfter.setOnClickListener {
+//            changeDataSet { items.addNewAt(itemSpinner.selectedItemPosition + 1) }
+//        }
+
+
+        binding.checkViewOfPreview.setOnClickListener {
+//            val id = this.layoutPosition
+//            onSelectionListener!!.onCheckBoxClick(itemList[id], it, id)
+
+            if (it is CheckView){
+                it.setChecked(true)
+            }
         }
-
-        buttonGoTo.setOnClickListener {
-            viewPager.setCurrentItem(itemSpinner.selectedItemPosition, true)
-        }
-
-
-
-        buttonRemove.setOnClickListener {
-            changeDataSet { items.removeAt(itemSpinner.selectedItemPosition) }
-        }
-
-        buttonAddBefore.setOnClickListener {
-            changeDataSet { items.addNewAt(itemSpinner.selectedItemPosition) }
-        }
-
-        buttonAddAfter.setOnClickListener {
-            changeDataSet { items.addNewAt(itemSpinner.selectedItemPosition + 1) }
-        }
-
         items.imageList.observe(this) {
             previewAdapter.setItems(it)
 
@@ -125,38 +135,38 @@ abstract class PreviewBaseActivity : FragmentActivity() {
 
     }
 
-    private fun changeDataSet(performChanges: () -> Unit) {
-        if (checkboxDiffUtil.isChecked) {
-            /** using [DiffUtil] */
-            val idsOld = items.createIdSnapshot()
-            performChanges()
-            val idsNew = items.createIdSnapshot()
-            DiffUtil.calculateDiff(object : DiffUtil.Callback() {
-                override fun getOldListSize(): Int = idsOld.size
-                override fun getNewListSize(): Int = idsNew.size
-
-                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-                    idsOld[oldItemPosition] == idsNew[newItemPosition]
-
-                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-                    areItemsTheSame(oldItemPosition, newItemPosition)
-            }, true).dispatchUpdatesTo(viewPager.adapter!!)
-        } else {
-            /** without [DiffUtil] */
-            val oldPosition = viewPager.currentItem
-            val currentItemId = items.itemId(oldPosition)
-            performChanges()
-            viewPager.adapter!!.notifyDataSetChanged()
-            if (items.contains(currentItemId)) {
-                val newPosition =
-                    (0 until items.size).indexOfFirst { items.itemId(it) == currentItemId }
-                viewPager.setCurrentItem(newPosition, false)
-            }
-        }
-
-        // item spinner update
-        (itemSpinner.adapter as BaseAdapter).notifyDataSetChanged()
-    }
+//    private fun changeDataSet(performChanges: () -> Unit) {
+//        if (checkboxDiffUtil.isChecked) {
+//            /** using [DiffUtil] */
+//            val idsOld = items.createIdSnapshot()
+//            performChanges()
+//            val idsNew = items.createIdSnapshot()
+//            DiffUtil.calculateDiff(object : DiffUtil.Callback() {
+//                override fun getOldListSize(): Int = idsOld.size
+//                override fun getNewListSize(): Int = idsNew.size
+//
+//                override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+//                    idsOld[oldItemPosition] == idsNew[newItemPosition]
+//
+//                override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+//                    areItemsTheSame(oldItemPosition, newItemPosition)
+//            }, true).dispatchUpdatesTo(viewPager.adapter!!)
+//        } else {
+//            /** without [DiffUtil] */
+//            val oldPosition = viewPager.currentItem
+//            val currentItemId = items.itemId(oldPosition)
+//            performChanges()
+//            viewPager.adapter!!.notifyDataSetChanged()
+//            if (items.contains(currentItemId)) {
+//                val newPosition =
+//                    (0 until items.size).indexOfFirst { items.itemId(it) == currentItemId }
+//                viewPager.setCurrentItem(newPosition, false)
+//            }
+//        }
+//
+//        // item spinner update
+//        (itemSpinner.adapter as BaseAdapter).notifyDataSetChanged()
+//    }
 
     val items: ItemsViewModel by viewModels()
 
